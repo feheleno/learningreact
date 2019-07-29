2726979f9bc91c118161ba87c9e1bf1b737b8bbc
Fernando Heleno <feheleno@gmail.com>
2019-07-29 00:20:48 -0300

    criado AddColorForm que permite selecionar uma cor e dar um título para ela

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import AddColorForm from './AddColorForm';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logColor = (title, color) =>
+    console.log(`New Color: ${title} | ${color}`)
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AddColorForm onNewColor={logColor} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
