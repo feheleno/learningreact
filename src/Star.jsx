966f5b730e21cbc2848e70586991d60d7ecd84eb
Fernando Heleno <feheleno@gmail.com>
2019-07-29 23:26:07 -0300

    criado o componente funcional stateless Star para representar uma estrela e seu respectivo css

---
+++ src/Star.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './star.css';
+
+const Star = ({selected=false, onClick=f=>f}) => 
+<div className={(selected)?'star selected' : 'star'}
+     onClick={onClick}>
+</div>
+
+Star.propTypes = {
+    selected: PropTypes.bool,
+    onClick: PropTypes.func
+}
