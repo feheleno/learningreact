1ffa202c32dc9e54bfed979706bcc9dac5de97ca
Fernando Heleno <feheleno@gmail.com>
2019-07-29 23:20:40 -0300

    transformado o componente AddColorForm em função stateless

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import AddColorForm from './AddColorForm';
 import * as serviceWorker from './serviceWorker';
 
-const logColor = (title, color) =>
-    console.log(`New Color: ${title} | ${color}`)
-
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<AddColorForm onNewColor={logColor} />, document.getElementById('root'));
+ReactDOM.render(<AddColorForm onNewColor={(title, color) => {
+    console.log(`TODO: add new ${title} and ${color} to the list`)
+    console.log(`TODO: render UI with new Color`)
+}} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
