6a72de880543c205d613f9b4229d1f59593d3acb
Fernando Heleno <feheleno@gmail.com>
2019-08-02 23:05:27 -0300

    Criando o starRating

---
+++ src/StarRating.jsx
@@ -0,0 +1,85 @@
+/*****************************************
+ *** StarRating utilizando createClass ***
+ *****************************************
+import React from 'react';
+
+const StarRating = createClass({
+    displayName: 'StarRating',
+    propTypes: {
+        totalStars: PropTypes.number
+    }, 
+    getDefaultProps() {
+        return {
+            totalStars: 5
+        }
+    },
+    getInitialState() {
+        return {
+            starsSelected: 0
+        }
+    },
+    change(starsSelected) {
+        this.setState({starsSelected})
+    },
+    render() {
+        const {totalStars} = this.props
+        const {starsSelected} = this.state
+        return (
+            <div className="star-rating">
+                {[...Array(totalStars)].map((n, i) => 
+                    <Star key={i}
+                          selected={i<starsSelected}
+                          onClick={() => this.change(i+1)}
+                    />
+                )}
+           <p>{starsSelected} of {totalStars} stars</p>
+        </div>
+    )
+  }
+})
+*/
+
+/********************************************
+ *** StarRating utilizando componente ES6 ***
+ ********************************************/
+import React, { Component } from 'react';
+
+class StarRating extends Component {
+  
+    constructor(props) {
+        super(props)
+        this.state = {
+            starsSelected: 0
+        }
+        this.change = this.change.bind(this)
+    }
+  
+    change(starsSelected) {
+        this.setState({starsSelected})
+    }
+  
+    render() {
+        const {totalStars} = this.props
+        const {starsSelected} = this.state
+        return (
+            <div className="star-rating">
+                {[...Array(totalStars)].map((n, i) => 
+                    <Star key={i}
+                          selected={i<starsSelected}
+                          onClick={() => this.change(i+1)}
+                    />
+                )}
+                <p>{starsSelected} of {totalStars} stars</p>
+            </div>
+        )
+    }
+  
+}
+
+StarRating.propTypes = {
+    totalStars: PropTypes.number
+}
+
+StarRating.defaultProps = {
+    totalStars: 5
+}
