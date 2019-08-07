bdea1bacaa36f51ca19e1dbd96d4e90f4e73d42b
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-08-07 17:52:37 -0300

    comentado import não utilizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/globals.scss';
-import AddColorForm from './AddColorForm';
+//import AddColorForm from './AddColorForm';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
