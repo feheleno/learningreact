ff55dc318008af3c94e4d1829b81f401f2eb879c
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-08-07 17:41:25 -0300

    Criados os métodos rateColor e removeColor na aplicação principal

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ class App extends Component {
       colors:[]
     }
     this.addColor = this.addColor.bind(this)
+    this.rateColor = this.rateColor.bind(this)
+    this.removeColor = this.removeColor.bind(this)
   }
 
   addColor(title, color){
@@ -27,17 +29,38 @@ class App extends Component {
     this.setState({colors})
   }
 
+  rateColor(id, rating){
+    const colors = this.state.colors.map(color =>
+      (color.id !== id) ?
+      color :
+      {
+        ...color,
+        rating
+      }
+    )
+    this.setState({colors})
+  }
+
+  removeColor(id){
+    const colors = this.state.colors.filter(
+      color => color.id !== id
+    )
+    this.setState({colors})
+  }
+
   render(){
-    const {addColor} = this
+    const {addColor, rateColor, removeColor} = this
     const {colors} = this.state
     return(
       <div className="app">
         <AddColorForm onNewColor={addColor} />
-        <ColorList colors={colors} />
+        <ColorList colors={colors}
+                   onRate={rateColor}
+                   onRemove={removeColor} />
       </div>
     )
   }
-  
+
 }
 
 export default App;
