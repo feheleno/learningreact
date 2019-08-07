aeb8ca30dad3026a38dab734b6844ab3c797ded7
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-08-07 17:45:06 -0300

    Alterado o componente que estava sendo renderizado

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/globals.scss';
 import AddColorForm from './AddColorForm';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<AddColorForm onNewColor={(title, color) => {
-    console.log(`TODO: add new ${title} and ${color} to the list`)
-    console.log(`TODO: render UI with new Color`)
-}} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<AddColorForm onNewColor={(title, color) => {
+//     console.log(`TODO: add new ${title} and ${color} to the list`)
+//     console.log(`TODO: render UI with new Color`)
+// }} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
