6563eaf2df54ac301d2c24abd05583e0e4b223c6
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-08-07 17:26:13 -0300

    ajustando erro no ColorList

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const ColorList = ({colors=[]}) =>
+const ColorList = ({colors=[], onRate=f=>f, onRemove=f=>f}) =>
 <div className=" color-list">
     {(colors.length === 0) ?
         <p>No Colors Listed. (Add a Color)</p> :
-        colors.map(color => <Color keyy{color.id}{...color} />)
+        colors.map(color => 
+            <Color key={color.id}{...color} />)
     }
 </div>
